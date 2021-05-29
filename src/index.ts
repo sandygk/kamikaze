3b218a160cac9c4e25833b15addb7f8e56ccb69e
Sandy <sandy.gutierrezkamayd@ge.com>
2021-05-28 21:25:27 -0400

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,72 @@
 import * as PIXI from "pixi.js";
 import "./style.css";
 
-declare const VERSION: string;
-
-const gameWidth = 800;
-const gameHeight = 600;
-
-console.log(`Welcome from pixi-typescript-boilerplate ${VERSION}`);
+const resolution = {
+  width: 800,
+  height: 600,
+};
 
 const app = new PIXI.Application({
-    backgroundColor: 0xd3d3d3,
-    width: gameWidth,
-    height: gameHeight,
+  backgroundColor: 0x000000,
+  width: resolution.width,
+  height: resolution.height,
+ 
 });
 
 const stage = app.stage;
 
 window.onload = async (): Promise<void> => {
-    await loadGameAssets();
+  await loadGameAssets();
 
-    document.body.appendChild(app.view);
+  document.body.appendChild(app.view);
 
-    resizeCanvas();
+  resizeCanvas();
+  window.addEventListener("resize", resizeCanvas);
 
-    const birdFromSprite = getBird();
-    birdFromSprite.anchor.set(0.5, 0.5);
-    birdFromSprite.position.set(gameWidth / 2, gameHeight / 2);
+  const birdFromSprite = getBird();
+  birdFromSprite.anchor.set(0.5, 0.5);
+  birdFromSprite.position.set(resolution.width / 2, resolution.height / 2);
 
-    stage.addChild(birdFromSprite);
+  stage.addChild(birdFromSprite);
 };
 
 async function loadGameAssets(): Promise<void> {
-    return new Promise((res, rej) => {
-        const loader = PIXI.Loader.shared;
-        loader.add("rabbit", "./assets/simpleSpriteSheet.json");
+  return new Promise((res, rej) => {
+    const loader = PIXI.Loader.shared;
+    loader.add("rabbit", "./assets/simpleSpriteSheet.json");
 
-        loader.onComplete.once(() => {
-            res();
-        });
-
-        loader.onError.once(() => {
-            rej();
-        });
-
-        loader.load();
+    loader.onComplete.once(() => {
+      res();
     });
+
+    loader.onError.once(() => {
+      rej();
+    });
+
+    loader.load();
+  });
 }
 
 function resizeCanvas(): void {
-    const resize = () => {
-        app.renderer.resize(window.innerWidth, window.innerHeight);
-        app.stage.scale.x = window.innerWidth / gameWidth;
-        app.stage.scale.y = window.innerHeight / gameHeight;
-    };
-
-    resize();
-
-    window.addEventListener("resize", resize);
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+  const heightRatio = window.innerHeight / resolution.height;
+  const widthRatio = window.innerWidth / resolution.width;
+  const scale = Math.min(heightRatio, widthRatio);
+  app.stage.scale.x = scale;
+  app.stage.scale.y = scale;
 }
 
 function getBird(): PIXI.AnimatedSprite {
-    const bird = new PIXI.AnimatedSprite([
-        PIXI.Texture.from("birdUp.png"),
-        PIXI.Texture.from("birdMiddle.png"),
-        PIXI.Texture.from("birdDown.png"),
-    ]);
+  const bird = new PIXI.AnimatedSprite([
+    PIXI.Texture.from("birdUp.png"),
+    PIXI.Texture.from("birdMiddle.png"),
+    PIXI.Texture.from("birdDown.png"),
+  ]);
 
     bird.loop = true;
-    bird.animationSpeed = 0.1;
-    bird.play();
-    bird.scale.set(3);
+  bird.animationSpeed = 0.1;
+  bird.play();
+  bird.scale.set(3);
 
-    return bird;
+  return bird;
 }
